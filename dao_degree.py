2fad9ebd7f6863709da4c28d724d2cb975cff5f4
Èrik Lledó <65349896+lledoerik@users.noreply.github.com>
2025-10-03 13:02:17 +0200

    Update and rename db_utils.py to dao_degree.py

---
+++ dao_degree.py
@@ -0,0 +1,69 @@
+from db_connection import connection_bd
+from degree import Degree
+
+
+def create_table(degree, user, password):
+    connection = connection_bd(user, password)
+    if connection.is_connected():
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO carreras (Nombre) VALUES (%s)"
+                val = (degree.get_name(),)
+                cursor.execute(sql, val)
+                connection.commit()
+                return True
+        except Exception as e:
+            return None
+        finally:
+            connection.close()
+    return None
+
+
+def show_table(table, user, password):
+    sql = f"SELECT * FROM {table}"
+    connection = connection_bd(user, password)
+    if connection.is_connected():
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                results = cursor.fetchall()
+                return results
+        except Exception as e:
+            return e
+        finally:
+            connection.close()
+
+
+def update_table(degree, user, password):
+    connection = connection_bd(user, password)
+    if connection.is_connected():
+        try:
+            with connection.cursor() as cursor:
+                sql = "UPDATE carreras SET Nombre = %s WHERE idCarrera = %s"
+                val = (degree.get_name(), degree.get_id())
+                cursor.execute(sql, val)
+                connection.commit()
+                return True
+        except Exception as e:
+            return None
+        finally:
+            connection.close()
+
+
+def delete_table(degree, user, password):
+    connection = connection_bd(user, password)
+    if connection.is_connected():
+        try:
+            with connection.cursor() as cursor:
+                sql = "DELETE FROM carreras WHERE Nombre = %s"
+                val = (degree.get_name(),)
+                cursor.execute(sql, val)
+                connection.commit()
+                cursor.close()
+                connection.close()
+                return True
+        except Exception as e:
+            return None
+
+        finally:
+            connection.close()
