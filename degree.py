f5d8001e9ef005f10ed5ff36c672e1906cf32435
Èrik Lledó <65349896+lledoerik@users.noreply.github.com>
2025-10-03 13:01:11 +0200

    Update and rename Carrera.py to degree.py

---
+++ degree.py
@@ -0,0 +1,24 @@
+class Degree:
+    def __init__(self, name, id=None):
+        self.set_name(name)
+        self.set_id(id)
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_id(self, id):
+        self.__id = id
+
+    def get_name(self):
+        return self.__name
+
+    def get_id(self):
+        return self.__id
+
+    @classmethod
+    def degrees_manager_menu(self):
+        print("1.Mostrar carreras")
+        print("2.Insertar carrera")
+        print("3.Actualizar carrera")
+        print("4.Eliminar carrera")
+        print("0.Volver al menu principal")
