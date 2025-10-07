ef566c9e62c4924be6e19f83790a3013e911cd28
Èrik Lledó <lledoerik@gmail.com>
2025-10-07 12:49:00 +0200

    functions updated in main and api

--- api.py
+++ api.py
@@ -1,13 +1,31 @@
 from flask import Flask,request,jsonify
-from dao_degree import update_degree, delete_degree, insert_degree, show_degrees
+from dao_degree import update_degree, delete_degree, insert_degree, show_degrees, dao_degree
+from db_connection import db_connection
+
 
 app = Flask(__name__)
+dao = dao_degree()
+
+@app.route("/login", methods=["GET"])
+def login():
+    user = request.args.get("user")
+    password = request.args.get("password")
+    con = db_connection(user,password)
+    conn = con.connection_bd()
+    dao.login(conn)
+    if conn.is_connected():
+        return jsonify({"login": True})
+    else:
+        return jsonify({"login": False})
 
 
 @app.route("/insert")
 def insert_degree_api():
-    return insert_degree()
-
+    degree = request.args.get("degree")
+    user = request.args.get("user")
+    password = request.args.get("password")
+    result = insert_degree(degree, user, password)
+    return jsonify({"status": "success", "message": "Degree inserted"})
 
 
 @app.route("/show", methods=["GET"])
@@ -15,18 +33,26 @@ def show_degrees_api():
     table = request.args.get("table")
     user = request.args.get("user")
     password = request.args.get("password")
-    array = show_degrees(table,user,password) 
-    return jsonify([d.to_dict() for d in array]) 
+    array = show_degrees(table, user, password)
+    return jsonify([d.to_dict() for d in array])
 
 
 @app.route("/update")
 def update_degree_api():
-    return update_degree()
+    table = request.args.get("table")
+    user = request.args.get("user")
+    password = request.args.get("password")
+    result = update_degree(table, user, password)
+    return jsonify({"status": "success", "message": "Degree updated"})
 
 
 @app.route("/delete")
 def delete_degree_api():
-    return delete_degree()
+    table = request.args.get("table")
+    user = request.args.get("user")
+    password = request.args.get("password")
+    result = delete_degree(table, user, password)
+    return jsonify({"status": "success", "message": "Degree deleted"})
 
 
 app.run()
--- main.py
+++ main.py
@@ -19,10 +19,9 @@ while not user_connection:
     user = input()
     print("dime tu contraseña de la bd")
     password = input()
-    connection = connection_bd(user, password)
-    if connection is not None or False:
+    con = req.get("http://localhost:5000/login", params={"user": user, "password": password})
+    if con is not None or False:
         print("Conexion establecida")
-        connection.close()
         user_connection = True
     else:
         print("Datos introduciodos incorrectos")
@@ -36,7 +35,8 @@ while select_menu_p != 0:
             Degree.degrees_manager_menu()
             degree_menu = int(input())
             if degree_menu == 1:
-                response = req.get("http://localhost:5000/show", params={"table": "carreras", "user": user, "password":password})
+                response = req.get("http://localhost:5000/show",
+                                   params={"table": "carreras", "user": user, "password": password})
                 if response.status_code == 200:
                     data = response.json()  # lista de diccionarios
                     degrees = [Degree.from_dict(item) for item in data]  # reconstruyes tus objetos
@@ -44,23 +44,37 @@ while select_menu_p != 0:
                         print(f"{d.get_id()}: {d.get_name()}")
                 else:
                     print("Error:", response.status_code)
+
             elif degree_menu == 2:
                 name = input("Inserta el nombre de la carrera a crear: ")
-                degree = Degree(name)
-                if insert_degree(degree, user, password):
+                response = req.get("http://localhost:5000/insert",
+                                   params={"degree": name, "user": user, "password": password})
+                if response.status_code == 200:
+                    data = response.json()
+                    degree = Degree(name)
                     print("Carrera insertada correctamente en la base de datos")
+                else:
+                    print("Error:", response.status_code)
+
             elif degree_menu == 3:
+                response = req.get("http://localhost:5000/update",
+                                   params={"table": "carreras", "user": user, "password": password})
                 print(show_degrees("carreras", user, password))
-                print("Inserta el id de la carrera que a modificar: ")
-                id_degree = int(input())
-                print("Indica el nuevo nombre de la carrera: ")
-                degree = Degree(input(), id_degree)
-                if update_degree(degree, user, password):
+                id_degree = int(input("Introduce el ID de la carrera a modificar: "))
+                new_name = input("Introduce el nuevo nombre de la carrera: ")
+                if response.status_code == 200:
+                    degree = Degree(new_name, id_degree)
                     print("Se ha actulizado el nombre de la carrera")
+                else:
+                    print("Error:", response.status_code)
 
             elif degree_menu == 4:
+                response = req.get("http://localhost:5000/delete",
+                                   params={"table": "carreras", "user": user, "password": password})
                 print(show_degrees("carreras", user, password))
-                print("Indica la carrera a eliminar: ")
-                degree = Degree(input().lower())
-                if delete_degree(degree, user, password):
+                degree_name = input("Indica la carrera a eliminar: ")
+                if response.status_code == 200:
+                    degree = Degree(input().lower())
                     print("Se ha eliminado la carrera")
+                else:
+                    print("Error: ", response.status_code)
