ab9b5178208b198daf78972196f663c7e28dcd32
Albert Grau <albert31072004@gmail.com>
2025-10-07 11:01:29 +0200

    funcion mostrar bd y tratamiento de obj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from api import show_degrees
 from degree import Degree
-from dao_degree import update_table, delete_table, create_table, show_table
+from dao_degree import update_degree, delete_degree, insert_degree, show_table
 from db_connection import connection_bd
 import requests as req
 
@@ -37,24 +36,31 @@ while select_menu_p != 0:
             Degree.degrees_manager_menu()
             degree_menu = int(input())
             if degree_menu == 1:
-                print(show_table("carreras", user, password))
+                response = req.get("http://localhost:5000/show", params={"table": "carreras", "user": user, "password":password})
+                if response.status_code == 200:
+                    data = response.json()  # lista de diccionarios
+                    degrees = [Degree.from_dict(item) for item in data]  # reconstruyes tus objetos
+                    for d in degrees:
+                        print(f"{d.get_id()}: {d.get_name()}")
+                else:
+                    print("Error:", response.status_code)
             elif degree_menu == 2:
                 name = input("Inserta el nombre de la carrera a crear: ")
                 degree = Degree(name)
-                if insert_degree(degree, user, password):
+                if create_table(degree, user, password):
                     print("Carrera insertada correctamente en la base de datos")
             elif degree_menu == 3:
-                print(show_degrees("carreras", user, password))
+                print(show_table("carreras", user, password))
                 print("Inserta el id de la carrera que a modificar: ")
                 id_degree = int(input())
                 print("Indica el nuevo nombre de la carrera: ")
                 degree = Degree(input(), id_degree)
-                if update_degree(degree, user, password):
+                if update_table(degree, user, password):
                     print("Se ha actulizado el nombre de la carrera")
 
             elif degree_menu == 4:
-                print(show_degrees("carreras", user, password))
+                print(show_table("carreras", user, password))
                 print("Indica la carrera a eliminar: ")
                 degree = Degree(input().lower())
-                if delete_degree(degree, user, password):
+                if delete_table(degree, user, password):
                     print("Se ha eliminado la carrera")
